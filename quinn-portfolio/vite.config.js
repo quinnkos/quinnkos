38659e61a927d33ab1499c054183808bea98e6d1
quinnkos <qtrk05@gmail.com>
2025-08-02 20:42:52 -0700

    deploy vite stuff

--- quinn-portfolio/vite.config.js
+++ quinn-portfolio/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/quinn-portfolio/', // 👈 This is important for GitHub Pages
   plugins: [react()],
 })
